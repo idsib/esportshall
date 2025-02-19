e69e17611f1511b9333ce58b3677eb0d3e42ece2
Jara <jjara@institutmvm.cat>
2025-02-19 16:39:14 +0100

    politicas de privacidad y cookies añadido

---
+++ src/app/cookies-policy.tsx
@@ -0,0 +1,53 @@
+const CookiesPolicy = () => {
+    return (
+      <div className="container-xxl py-5">
+        <div className="container">
+          <div className="row g-5 justify-content-center">
+            <div className="col-lg-7 text-start">
+              <h1 className="bs-primary text-center">POLÍTICA DE COOKIES ESPORTS HALL</h1>
+              <br />
+              <div className="mb-4 text-white">
+                <h3>1. Uso de cookies</h3>
+                <br />
+                El sitio web, propiedad de Esports Hall y ubicado en Av. d'Eduard Maristany, 59, 08930 Sant Adrià de Besòs, Barcelona, con número de CIF: ESQ5856078J, utiliza un sistema de cookies...
+                <br /><br />
+                <h3>2. Consentimiento</h3>
+                <br />
+                Las cookies que utilizamos no almacenan datos personales ni ningún tipo de información que pueda identificarte.
+                <br /><br />
+                <h3>3. Tipos y Propósito de las Cookies</h3>
+                <br />
+                Las cookies, según su permanencia, pueden clasificarse en:  
+                Cookies de sesión: expiran cuando el usuario cierra el navegador.
+                <br />
+                Las cookies, según su propósito, pueden clasificarse de la siguiente manera:
+                <ul>
+                  <li>Cookies de geolocalización: Estas cookies se utilizan para determinar el país en el que te encuentras...</li>
+                  <li>Cookies de registro (técnicas y de personalización): Estas cookies se generan una vez que el usuario se ha registrado...</li>
+                  <li>Cookies analíticas: Cada vez que un usuario visita un servicio o información en el sitio web...</li>
+                  <li>Cookies de publicidad propia: Se utilizan para gestionar si un usuario ha visitado la publicidad...</li>
+                  <li>Cookies de publicidad de terceros: Estos terceros pueden almacenar cookies enviadas desde el sitio web...</li>
+                </ul>
+                <br /><br />
+                <h3>4. Cómo bloquear o eliminar las cookies instaladas</h3>
+                <br />
+                El usuario puede permitir, bloquear o eliminar las cookies instaladas en su dispositivo a través de la configuración de su navegador. Puedes encontrar información para los navegadores más comunes:
+                <ul>
+                  <li>Explorer: <a href="https://support.microsoft.com/es-es/kb/278835">support.microsoft.com</a></li>
+                  <li>Chrome: <a href="http://support.google.com/chrome/bin/answer.py?hl=es&answer=95647">support.google.com</a></li>
+                  <li>Firefox: <a href="https://support.mozilla.org/es/kb/Borrar%20cookies">support.mozilla.org</a></li>
+                  <li>Safari: <a href="http://support.apple.com/kb/ph5042">support.apple.com</a></li>
+                </ul>
+                <br /><br />
+                <h3>5. Modificaciones</h3>
+                <br />
+                El sitio web, propiedad de Esports Hall, ubicado en Av. d'Eduard Maristany, 59, 08930 Sant Adrià de Besòs, Barcelona, España, con número de CIF: ESQ5856078J, puede modificar esta política de cookies en función de requisitos legales...
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default CookiesPolicy;
