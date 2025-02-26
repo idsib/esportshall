a3dd5746ec77c34c773995a7d80c9ed6b0e96d7b
MajestadAyman <abourhim@institutmvm.cat>
2025-02-26 16:56:03 +0100

    first steps for encode password (bcrypt)

--- backend/main.js
+++ backend/main.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 export const GetUsers = async () => {
     await fetch('http://localhost:5000/users'
     .then(response => {
@@ -6,4 +9,18 @@ export const GetUsers = async () => {
     .then(data => {
       setUsers(data);
     }))
+  };
+
+  export const registerUserInBackend = async (name, surname, email, password) => {
+    const completeName = name + " " + surname;
+  
+    await fetch('http://localhost:3000/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token,
+      },
+
+      body: JSON.stringify(userData),
+    });
   };
